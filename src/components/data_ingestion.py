ea51cb58f44c5fa9256e15619f4781ae66bd4a83
raw9k <raunakgupta914@gmail.com>
2025-06-27 23:14:53 +0530

    update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,4 +13,6 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from dataclasses import dataclass  
+from dataclasses import dataclass
+
+@dataclass
