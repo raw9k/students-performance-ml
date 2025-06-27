34e2ab77db3c06ae46247e54fcd4a8660b41c130
raw9k <raunakgupta914@gmail.com>
2025-06-27 23:12:48 +0530

    Update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,3 +4,13 @@ database.
 It will have all the code related to reading the data
 
 """
+import sys
+import os
+
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass 
