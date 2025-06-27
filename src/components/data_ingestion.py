03966ace6aaed632b98731ed0d555aad3ca221a4
raw9k <raunakgupta914@gmail.com>
2025-06-28 00:12:32 +0530

    update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -16,3 +16,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
+class DataIngestion_config:
+    train_data_path: str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+    raw_data_path:str=os.path.join("artifacts","data.csv")
