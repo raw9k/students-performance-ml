a0daba0d9921ad6ea82b275ac52b1cda64c7d1c1
raw9k <raunakgupta914@gmail.com>
2025-06-28 02:20:16 +0530

    update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,8 @@ import os
 from src.exception import CustomException
 from src.logger import logging
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
+import pandas as pd # type: ignore
+from sklearn.model_selection import train_test_split # type: ignore
 
 from dataclasses import dataclass
 
