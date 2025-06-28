6f7c03932a124a230bb871f32ae459e944575cdd
raw9k <raunakgupta914@gmail.com>
2025-06-28 22:43:37 +0530

    update

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -2,3 +2,16 @@
 2. It will have the code related to transformation like
 one hot encoding, label encoding etc.
 """
+
+import sys
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
